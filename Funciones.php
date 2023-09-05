2cd4cf677ccac27a5efbc918681eb34b6444cb37
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2023-09-04 21:26:36 -0500

    Changes add Files

--- Funciones.php
+++ Funciones.php
@@ -1,4 +1,19 @@
 <?php
+// Funciones de Php
 
-// Funciones en php
+function foo($arg_1, $arg_2, /* ..., */ $arg_n)
+{
+    echo "Función de ejemplo.\n";
+    return $valor_devuelto;
+}
 
+// Funcion con un Condiccional
+
+if ($hacer_algo) {
+    function foo()
+    {
+      echo "No existo hasta que la ejecución del programa llegue hasta mí.\n";
+    }
+  }
+
+?>
--- Tipos-de-Datos.php
+++ Tipos-de-Datos.php
@@ -1,4 +1,42 @@
 <?php
+// Tipos de Datos
 
-// Tipos de Datos en php
+// Tipo Booleanos
 
+$vari = True; // Asigna el valor True a $vari
+$ables = false; // Asigna el Valor False a $ables
+
+// Tipos de Datos - Numeros
+
+// Numeros Interger
+
+$a = 1234; // Un Número Decimal
+$a = -123; // Un Número Negativo
+$a = 0123; // Un Número Octal - Equivale a 83 Decimal
+$a = 0x1A; // número hexadecimal - Equivale a 26 Decimal
+$a = 0b11111111; // Un Número Binario - Equivale al 255 Decimal
+
+// Numeros Float
+
+$a = 1.234; 
+$b = 1.2e3; 
+$c = 7E-10;
+$d = 1_234.567; // A partir de Php 7.4.0
+
+// Tipos de Datos - Array
+
+// Formas de Array
+
+$array = array(
+    "foo" => "bar",
+    "bar" => "foo",
+);
+
+// A partir de Php 5.4
+
+$array = [
+    "foo" => "bar",
+    "bar" => "foo",
+];
+
+?>
