e15cb0f40ca93b4bf7bd9350c89ff89f4b2d3f7a
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2023-09-06 21:42:50 -0500

    Changes Bucles - Php

--- Bucles.php
+++ Bucles.php
@@ -1,4 +1,42 @@
 <?php
 
-// Bucles en php
+// Bucles en Php
 
+$galletas = 0;
+
+while ($galletas < 10){
+
+echo "$galletas, ";
+
+$galletas++;
+
+}
+
+$nume1 = 0;
+
+while ($nume1 < 50){
+    echo "$nume1, ";
+    $nume1++;
+}
+
+// Bucles Do - While
+
+$galle = 0;
+
+do {
+
+echo "$galle, ";
+
+$galle++;
+
+} while ($galle < 10);
+
+// Bucle For
+
+for ($etas = 0; $etas < 10; $etas++){
+
+    echo "$etas, ";
+
+    }
+
+?>
