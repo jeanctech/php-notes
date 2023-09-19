97dcbfd39a48af5f5934180a956c8d2970f70827
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2023-09-18 19:00:25 -0500

    Changes in Files - Condiccionales - Php

--- Cadena-de-Texto.php
+++ Cadena-de-Texto.php
@@ -7,7 +7,7 @@ echo 'También se pueden incluir nuevas líneas en
 un string de esta forma, ya que es
 correcto hacerlo así';
 
-// Tipos de Datos - Numeric String 
+// Tipos de Datos - Numeric String
 
 var_dump("0D1" == "000"); // false, "0D1" is not scientific notation
 var_dump("0E1" == "000"); // true, "0E1" is 0 * (10 ^ 1), or 0
--- Condiccionales.php
+++ Condiccionales.php
@@ -2,3 +2,65 @@
 
 // Condiccionales en php
 
+// Condiccional If - Else
+
+$lentes = true;
+
+if ($lentes) {
+    echo "Si estas usando lentes";
+}else {
+    echo "No estas utilizando lentes";
+}
+
+// Condiccional Elseif
+
+ //Lentes = 1 Si uso lentes
+ //Lentes = 2 Si uso lentes pero solo para leer
+ //Lentes = 3 No uso lentes
+
+ $lentes = 2;
+
+ if ( $lentes == 1 ){
+    echo "Si uso lentes"; /*Primer If*/
+}elseif( $lentes == 2 ){
+    echo " - Si uso lentes pero solo para leer"; /*Segundo If*/
+}else{
+    echo"No Uso Lentes"; /*Tercer If*/
+}
+
+// Condiccional Switch
+
+$temperatura = 23;
+
+switch ($temperatura) {
+case 10:
+echo "Ambiente Frio";
+break;
+
+case 15:
+echo "Ambiente Templado";
+break;
+
+case 23:
+echo " - Ambiente Agradable";
+break;
+
+case 34:
+echo "Ambiente con calor semi intenso";
+break;
+
+default:
+echo "No tengo lectura de calor";
+break;
+}
+
+// Condiccional Ternaria
+
+$A = "";
+$B = "";
+
+$respuesta = ($A == $B) ? "A es igual a B" : "A no es igual a B";
+
+echo ($A > $B) ? "A es mayor que B" : " - B es menor o igual que A";
+
+?>
